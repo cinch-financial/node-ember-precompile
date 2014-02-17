8e2677647ca0967c9b8af7dc044278f9db8c0e94
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2014-02-17 12:16:36 -0500

    ember -> 1.4.0

--- src/context.js
+++ src/context.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var vm = require('vm');
 
 var HANDLEBARSJS = fs.readFileSync(__dirname + '/../vendor/handlebars-1.3.0.js', 'utf8')
-var EMBERJS = fs.readFileSync(__dirname + '/../vendor/ember-1.3.1.js', 'utf8')
+var EMBERJS = fs.readFileSync(__dirname + '/../vendor/ember-1.4.0.js', 'utf8')
 
 function getBaseSandbox() {
 
